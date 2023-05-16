7d96438433e63649f63b187d831fc3fef0e573c6
EnderSlash2008 <133682928+EnderSlash2008@users.noreply.github.com>
2023-05-16 09:31:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 @app.route('JohnC')
-def hello(JohnC):
-    return render_template('page.html', JohnC)
+def hello():
+    return render_template('JohnC.html', JohnC)
